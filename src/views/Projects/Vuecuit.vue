07b6e1c83a3df46c5da1de14b27433a5c62cc7ba
iris <iris@heyiris.dev>
2025-01-05 04:40:34 +0000

    feat: add placeholder content to other views

---
+++ src/views/Projects/Vuecuit.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="page">
+    <h2>vuecuit</h2>
+  </main>
+</template>
--- src/views/UsesView.vue
+++ src/views/UsesView.vue
@@ -1,5 +1,51 @@
+<script setup lang="ts">
+const updatedMs = 1736022517 * 1000
+const updated = new Date(updatedMs).toLocaleDateString('en-GB', {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+})
+</script>
+
 <template>
-  <div class="about"></div>
+  <main class="page">
+    <h2>things that i use</h2>
+    <div class="last-updated">last updated: {{ updated }}</div>
+    <hr />
+
+    <section id="hardware">
+      <h3>hardware</h3>
+      <ul>
+        <li>my phone is a google pixel 7a running on calyxos</li>
+        <li>
+          my desktop is a modified prebuilt dell optiplex 7010, running with an i5-3470, 16gb of
+          ram, and an amd radeon rx 6400. it runs arch linux with the niri wayland compositor.
+        </li>
+        <li>
+          i have two monitors, both 1080p@60hz. one is a dell p2217h and the other is a dell... some
+          other model with a weird name.
+        </li>
+        <li>my keyboard is a keyboard, mouse is a mouse, etc.</li>
+      </ul>
+    </section>
+  </main>
 </template>
 
-<style></style>
+<style scoped lang="scss">
+.last-updated {
+  font-size: 0.75rem;
+  color: hsla(var(--text) / 0.5);
+}
+
+h3 {
+  font-size: 1.25rem;
+}
+
+ul {
+  padding: 0 0 0 1rem;
+  li {
+    margin-bottom: 0.5rem;
+    font-size: 0.9rem;
+  }
+}
+</style>
