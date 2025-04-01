6cdd8a8b24f97028c0be5cb2e0ddbaad763bc17b
iris <iris@heyiris.dev>
2025-04-01 16:55:05 +0100

    feat: add additional options to command palette, change visuals

--- src/components/CommandPalette.vue
+++ src/components/CommandPalette.vue
@@ -3,6 +3,9 @@ import { ref, onMounted, computed, watch, nextTick } from 'vue'
 import { useRouter } from 'vue-router'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { useThemeStore, type Theme } from '@/stores/theme'
+
+const themeStore = useThemeStore()
 
 const router = useRouter()
 const isOpen = ref(false)
@@ -30,48 +33,128 @@ type Command = {
 	action: () => void
 }
 
+const allCommands = () => {
+	const routesObject = {
+		name: 'routes',
+		commands: router
+			.getRoutes()
+			.filter((route) => route.meta.nav || route.name === 'home')
+			.map((route) => ({
+				id: route.name as string,
+				name: route.name as string,
+				description: `navigate to ${String(route.name)}`,
+				action: () => router.push(route.path),
+			})),
+	}
+
+	const themeObject = {
+		name: 'themes',
+		commands: ['latte', 'frappe', 'macchiato', 'mocha'].map((theme) => ({
+			id: `theme-${theme}`,
+			name: `theme: ${theme}`,
+			description: `switch to ${theme} (${theme === 'latte' ? 'light' : 'dark'})`,
+			action: () => themeStore.setTheme(theme as Theme),
+		})),
+	}
+
+	const socialObject = {
+		name: 'social',
+		commands: [
+			{
+				id: 'github',
+				name: 'github',
+				description: 'open github profile',
+				action: () => window.open('https://github.com/legallyiris', '_blank'),
+			},
+			{
+				id: 'bluesky',
+				name: 'bluesky',
+				description: 'open bluesky profile',
+				action: () => window.open('https://bsky.app/profile/legallyiris.dev', '_blank'),
+			},
+		],
+	}
+
+	const utilityObject = {
+		name: 'utility',
+		commands: [
+			{
+				id: 'share',
+				name: 'share page',
+				description: 'copy current url to clipboard',
+				action: () => {
+					navigator.clipboard.writeText(window.location.href)
+				},
+			},
+		],
+	}
+
+	return {
+		routes: routesObject,
+		theme: themeObject,
+		social: socialObject,
+		utility: utilityObject,
+	}
+}
+
 const commands = computed(() => {
-	const routes = router
-		.getRoutes()
-		.filter((route) => route.meta.nav || route.name === 'home')
-		.map((route) => ({
-			id: route.name as string,
-			name: route.name as string,
-			description: `navigate to ${String(route.name)}`,
-			action: () => router.push(route.path),
-		}))
+	const all = allCommands()
+	if (!searchQuery.value) return all
 
-	if (!searchQuery.value) return routes
+	const filtered = Object.entries(all).reduce(
+		(acc, [key, category]) => {
+			const filteredCommands = category.commands.filter(
+				(cmd) =>
+					cmd.name.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
+					cmd.description.toLowerCase().includes(searchQuery.value.toLowerCase()),
+			)
 
-	return routes.filter(
-		(command) =>
-			command.name.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
-			command.description.toLowerCase().includes(searchQuery.value.toLowerCase()),
+			if (filteredCommands.length > 0) {
+				acc[key] = {
+					name: category.name,
+					commands: filteredCommands,
+				}
+			}
+
+			return acc
+		},
+		{} as Record<string, { name: string; commands: Command[] }>,
 	)
+
+	return filtered
 })
+
+const flattenedCommands = computed(() => {
+	return Object.values(commands.value).flatMap((category) => category.commands)
+})
+const selectedCommand = ref<Command | null>(flattenedCommands.value[0])
 
 const handleKeyDown = (e: KeyboardEvent) => {
 	if (!isOpen.value) return
 
 	if (e.key === 'ArrowDown') {
 		e.preventDefault()
-		selectedIndex.value = (selectedIndex.value + 1) % commands.value.length
+		selectedIndex.value = (selectedIndex.value + 1) % flattenedCommands.value.length
+		selectedCommand.value = flattenedCommands.value[selectedIndex.value]
 	} else if (e.key === 'ArrowUp') {
 		e.preventDefault()
-		selectedIndex.value = (selectedIndex.value - 1 + commands.value.length) % commands.value.length
+		selectedIndex.value =
+			(selectedIndex.value - 1 + flattenedCommands.value.length) % flattenedCommands.value.length
+		selectedCommand.value = flattenedCommands.value[selectedIndex.value]
 	} else if (e.key === 'Enter') {
 		e.preventDefault()
-		if (commands.value[selectedIndex.value]) {
-			commands.value[selectedIndex.value].action()
+		if (flattenedCommands.value[selectedIndex.value]) {
+			flattenedCommands.value[selectedIndex.value].action()
 			isOpen.value = false
 			searchQuery.value = ''
+			selectedCommand.value = null
+			selectedIndex.value = 0
 		}
 	} else if (e.key === 'Escape') {
 		e.preventDefault()
 		isOpen.value = false
 		searchQuery.value = ''
 	} else if (e.key === 'Tab') {
-		// focus trap
 		e.preventDefault()
 		if (e.shiftKey) {
 			if (document.activeElement === searchInput.value) {
@@ -88,6 +171,11 @@ const handleKeyDown = (e: KeyboardEvent) => {
 			}
 		}
 	}
+
+	selectedCommand.value = flattenedCommands.value[selectedIndex.value]
+	const selectedCommandElement = document.getElementById(selectedCommand.value.id)
+	if (selectedCommandElement)
+		selectedCommandElement.scrollIntoView({ behavior: 'instant', block: 'center' })
 }
 
 watch(searchQuery, () => {
@@ -106,53 +194,97 @@ onMounted(() => {
 	window.addEventListener('keydown', handleKeyDown)
 })
 
-function executeCommand(command: Command) {
-	command.action()
-	isOpen.value = false
-	searchQuery.value = ''
+function executeCommand(index?: number) {
+	selectedIndex.value = index || selectedIndex.value
+	if (flattenedCommands.value[selectedIndex.value]) {
+		flattenedCommands.value[selectedIndex.value].action()
+		isOpen.value = false
+		searchQuery.value = ''
+		selectedCommand.value = null
+		selectedIndex.value = 0
+	}
 }
 </script>
 
 <template>
-	<div class="command-palette" :class="{ open: isOpen }">
-		<div class="overlay" @click="isOpen = false" v-if="isOpen"></div>
-		<div class="command-dialog" v-if="isOpen" ref="commandDialog">
-			<div class="command-input">
-				<FontAwesomeIcon :icon="faMagnifyingGlass" />
-				<input
-					ref="searchInput"
-					type="text"
-					v-model="searchQuery"
-					placeholder="search commands..."
-					@blur="
-						(e) => {
-							if (commandDialog?.contains(e.relatedTarget as Node)) {
-								e.preventDefault()
-							}
-						}
-					"
-				/>
-			</div>
-			<div class="command-results">
-				<div
-					v-for="(command, index) in commands"
-					:key="command.id"
-					class="command-item"
-					:class="{ active: index === selectedIndex }"
-					@click="executeCommand(command)"
-					tabindex="0"
-					@keydown.enter="executeCommand(command)"
-				>
-					<div class="command-name">{{ command.name }}</div>
-					<div class="command-description">{{ command.description }}</div>
+	<Teleport to="body">
+		<div class="command-palette" :class="{ open: isOpen }">
+			<Transition name="overlay-fade">
+				<div class="overlay" @click="isOpen = false" v-if="isOpen"></div>
+			</Transition>
+			<Transition name="dialog-slide">
+				<div class="command-dialog-wrapper" v-if="isOpen">
+					<div class="command-dialog" v-if="isOpen" ref="commandDialog">
+						<div class="command-input">
+							<FontAwesomeIcon :icon="faMagnifyingGlass" />
+							<input
+								ref="searchInput"
+								type="text"
+								v-model="searchQuery"
+								placeholder="search commands..."
+								@blur="
+									(e) => {
+										if (commandDialog?.contains(e.relatedTarget as Node)) {
+											e.preventDefault()
+										}
+									}
+								"
+							/>
+						</div>
+						<div class="command-results">
+							<div class="scroll-container" v-if="flattenedCommands.length > 0">
+								<div v-for="category in commands" :key="category.name" class="category-container">
+									<div class="category-label">{{ category.name }}</div>
+									<div class="commands">
+										<div
+											v-for="command in category.commands"
+											:id="command.id"
+											:key="command.id"
+											:class="[
+												'command-container',
+												{ selected: command.id === selectedCommand?.id },
+											]"
+											@click="
+												executeCommand(flattenedCommands.findIndex((c) => c.id === command.id))
+											"
+											@onkeydown="handleKeyDown($event)"
+										>
+											<div class="command-name">{{ command.name }}</div>
+											<div class="command-description">{{ command.description }}</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="scroll-container" v-else>
+								<div class="category-label">no commands found.</div>
+							</div>
+						</div>
+					</div>
 				</div>
-			</div>
+			</Transition>
 		</div>
-	</div>
+	</Teleport>
 </template>
 
 <style scoped lang="scss">
 @use '@/css/_variables.scss' as *;
+
+.command-dialog-wrapper {
+	position: fixed;
+	top: 20%;
+	left: 50%;
+	transform: translateX(-50%);
+	width: 90%;
+	max-width: 600px;
+	z-index: 1001;
+}
+
+.command-dialog {
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+	width: 100%;
+}
 
 .command-palette {
 	.overlay {
@@ -161,31 +293,18 @@ function executeCommand(command: Command) {
 		left: 0;
 		right: 0;
 		bottom: 0;
-		background: hsla(var(--crust) / 0.7);
-		backdrop-filter: blur(5px);
+		background: hsla(var(--mantle), 0.25);
+		backdrop-filter: blur(0.5rem) brightness(0.75);
 		z-index: 1000;
-	}
-
-	.command-dialog {
-		position: fixed;
-		top: 20%;
-		left: 50%;
-		transform: translateX(-50%);
-		width: 90%;
-		max-width: 600px;
-		background: hsl(var(--base));
-		border: 1px solid hsla(var(--overlay0) / 0.8);
-		border-radius: 0.5rem;
-		z-index: 1001;
-		box-shadow: 0 10px 25px rgba(0, 0, 0, 0.2);
-		overflow: hidden;
 	}
 
 	.command-input {
 		display: flex;
 		align-items: center;
 		padding: 0.75rem 1rem;
-		border-bottom: 1px solid hsla(var(--overlay0) / 0.3);
+		background: hsl(var(--base));
+		border: 1px solid hsla(var(--overlay2) / 0.25);
+		border-radius: 50rem;
 
 		svg {
 			color: hsla(var(--subtext0) / 0.8);
@@ -207,30 +326,117 @@ function executeCommand(command: Command) {
 	}
 
 	.command-results {
-		max-height: 60vh;
-		overflow-y: auto;
+		.scroll-container {
+			flex: 1;
+			overflow-y: auto;
+			max-height: 60dvh;
 
-		.command-item {
-			padding: 0.75rem 1rem;
-			cursor: pointer;
-			transition: $transition;
+			display: flex;
+			flex-direction: column;
+			gap: 0.5rem;
 
-			&.active,
-			&:hover {
-				background: hsla(var(--overlay0) / 0.3);
+			padding: 0.75rem;
+			background: hsl(var(--base));
+
+			border: 1px solid hsla(var(--overlay2) / 0.25);
+			border-radius: 1rem;
+
+			scrollbar-width: thin;
+			scrollbar-color: hsla(var(--overlay2) / 0.25) transparent;
+
+			&::-webkit-scrollbar {
+				width: 0.5rem;
 			}
 
-			.command-name {
-				font-size: 0.9rem;
-				font-weight: 600;
-				color: hsla(var(--text) / 0.9);
+			&::-webkit-scrollbar-track {
+				background: transparent;
 			}
 
-			.command-description {
-				font-size: 0.75rem;
-				color: hsla(var(--subtext0) / 0.8);
+			&::-webkit-scrollbar-thumb {
+				background: hsla(var(--overlay2) / 0.25);
+			}
+		}
+
+		.category-container {
+			max-height: 100%;
+			border: 1px solid hsla(var(--overlay1) / 0.25);
+			border-radius: 1rem;
+			background: hsl(var(--mantle));
+
+			.category-label {
+				font-weight: bold;
+				margin-bottom: 0.5rem;
+				font-size: 1.2rem;
+				margin: 0.25rem 1rem;
+			}
+
+			.commands {
+				border-radius: 0 0 1rem 1rem;
+				overflow: hidden;
+			}
+
+			.command-container {
+				position: relative;
+				padding: 0.5rem 1rem;
+				transition: background 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+
+				.command-name {
+					font-weight: bolder;
+				}
+
+				.command-description {
+					color: hsla(var(--subtext0) / 0.8);
+				}
+
+				&::after {
+					content: '';
+					position: absolute;
+					top: 0;
+					left: 50%;
+					transform: translateX(-50%);
+					width: 95%;
+					height: 1px;
+					background: hsla(var(--overlay1) / 0.25);
+					opacity: 1;
+					transition: opacity 0.2s ease-out;
+				}
+
+				&:hover {
+					background: hsla(var(--overlay0) / 0.25);
+					cursor: pointer;
+					transition: none;
+				}
+
+				&.selected {
+					background: hsla(var(--overlay0) / 0.5);
+					transition: none;
+					&:hover {
+						background: hsla(var(--overlay0) / 0.75);
+					}
+				}
 			}
 		}
 	}
+}
+
+.overlay-fade-enter-active,
+.overlay-fade-leave-active {
+	transition: 0.2s ease-out;
+}
+.overlay-fade-enter-from,
+.overlay-fade-leave-to {
+	opacity: 0;
+}
+
+.dialog-slide-enter-active,
+.dialog-slide-leave-active {
+	transition: all 0.2s ease-out;
+}
+
+.dialog-slide-enter-from,
+.dialog-slide-leave-to {
+	opacity: 0;
+	filter: blur(1rem);
+	transform: translate(-50%, 3rem);
 }
 </style>
