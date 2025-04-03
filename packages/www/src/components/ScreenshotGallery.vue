5ac94cefcc466b640f37debd605e5e329b7bb7a7
iris <iris@heyiris.dev>
2025-04-03 21:01:53 +0100

    refactor: restructure into a monorepo

---
+++ packages/www/src/components/ScreenshotGallery.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const props = defineProps<{
+  screenshots: { url: string; alt: string }[]
+}>()
+</script>
+
+<template>
+  <div class="screenshot-gallery">
+    <div class="screenshot" v-for="screenshot in props.screenshots" :key="screenshot.url">
+      <img :src="screenshot.url" :alt="screenshot.alt" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use '../css/_variables.scss' as *;
+
+.screenshot-gallery {
+  width: 100%;
+  display: flex;
+  overflow-x: auto;
+  gap: 0.5rem;
+  border-radius: 1rem;
+
+  .screenshot {
+    flex: 0 0 auto;
+    display: flex;
+    align-items: center;
+    max-width: 100%;
+
+    img {
+      max-height: 468px;
+      max-width: 100%;
+      height: auto;
+      width: auto;
+      border-radius: 1rem;
+      display: block;
+    }
+  }
+}
+</style>
---
+++ packages/www/src/views/Writing/WritingRoot.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+import { usePostsStore } from '@/stores/posts'
+import ContentCard from '@/components/card/ContentCard.vue'
+import ContentGrid from '@/components/card/ContentGrid.vue'
+
+const postsStore = usePostsStore()
+const posts = computed(() => postsStore.posts)
+postsStore.loadPosts()
+</script>
+
+<template>
+  <h1>writing</h1>
+  <p>i write about things that interest me sometimes</p>
+  <hr />
+
+  <ContentGrid v-if="posts.length">
+    <ContentCard
+      v-for="post in posts"
+      :key="post.slug"
+      :title="post.title"
+      :description="post.description"
+      :link="`/writing/${post.slug}`"
+      :meta="{
+        date: new Date(post.date).toLocaleDateString(),
+        readingTime: post.readingTime,
+      }"
+    />
+  </ContentGrid>
+  <div v-else>
+    <h3>no posts found</h3>
+    <p>check back later.</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use '../../css/variables.scss' as *;
+</style>
