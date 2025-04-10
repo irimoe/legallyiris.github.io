b3056f78c55b2ec2b233486c8e4bf552265d44fb
willow <willow@iri.moe>
2025-04-10 18:03:15 +0100

    chore: formatting

--- packages/www/src/views/Writing/WritingRoot.vue
+++ packages/www/src/views/Writing/WritingRoot.vue
@@ -10,32 +10,32 @@ const posts = computed(() => postsStore.postsMetadata)
 const loading = ref(true)
 
 onMounted(async () => {
-  loading.value = true
-  await postsStore.fetchPostsMetadata()
-  loading.value = false
+	loading.value = true
+	await postsStore.fetchPostsMetadata()
+	loading.value = false
 })
 </script>
 
 <template>
-  <h1>writing</h1>
-  <p>i write about things that interest me sometimes</p>
-  <hr />
+	<h1>writing</h1>
+	<p>i write about things that interest me sometimes</p>
+	<hr />
 
-  <div v-if="loading">loading posts...</div>
-  <ContentGrid v-else-if="posts.length">
-    <ContentCard
-      v-for="post in posts"
-      :key="post.slug"
-      :title="post.title"
-      :description="post.description"
-      :link="`/writing/${post.slug}`"
-      :meta="{ date: new Date(post.date).toLocaleDateString() }"
-    />
-  </ContentGrid>
-  <div v-else>
-    <h3>no posts found</h3>
-    <p>check back later, or maybe the api is down?</p>
-  </div>
+	<div v-if="loading">loading posts...</div>
+	<ContentGrid v-else-if="posts.length">
+		<ContentCard
+			v-for="post in posts"
+			:key="post.slug"
+			:title="post.title"
+			:description="post.description"
+			:link="`/writing/${post.slug}`"
+			:meta="{ date: new Date(post.date).toLocaleDateString() }"
+		/>
+	</ContentGrid>
+	<div v-else>
+		<h3>no posts found</h3>
+		<p>check back later, or maybe the api is down?</p>
+	</div>
 </template>
 
 <style scoped lang="scss">
--- packages/www/vite.dev.config.ts
+++ packages/www/vite.dev.config.ts
@@ -3,15 +3,15 @@ import { defineConfig, mergeConfig } from 'vite'
 import baseConfig from './vite.config'
 
 export default mergeConfig(
-  baseConfig,
-  defineConfig({
-    server: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:3000',
-          changeOrigin: true,
-        },
-      },
-    },
-  }),
+	baseConfig,
+	defineConfig({
+		server: {
+			proxy: {
+				'/api': {
+					target: 'http://localhost:3000',
+					changeOrigin: true,
+				},
+			},
+		},
+	}),
 )
